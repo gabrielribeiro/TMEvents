f0ea08ab4ce6cb160bb26f7bb25f7a6b20ce3dff
Gabriel Ribeiro <gabrielribeiro@live.com>
2023-11-08 04:34:14 -0300

    Add detail

--- TMEvents/Features/Detail/EventDetailViewController.swift
+++ TMEvents/Features/Detail/EventDetailViewController.swift
@@ -7,40 +7,70 @@
 
 import UIKit
 
-class EventDetailViewController: UIViewController {
+class EventDetailViewController: UITableViewController, EventDetailViewControllerDelegate {
+    
+    private let viewModel = EventDetailViewModel()
+    
+    private var eventImageView: UIImageView?
+    
+    private static let reuseIdentifier = "reuseIdentifier"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.title = "Detail"
+        viewModel.delegate = self
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: Self.reuseIdentifier)
+        tableView.tableFooterView = UIView()
     }
     
     func configure(for event: Event) {
-        self.label.text = event.name
+        title = event.name
+        
+        viewModel.setData(for: event)
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.items.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Self.reuseIdentifier, for: indexPath)
+
+        let row = viewModel.items[indexPath.row]
+        
+        var content = cell.defaultContentConfiguration()
+        
+        content.text = row.value
+        content.secondaryText = row.rowType.title
+        
+        cell.contentConfiguration = content
+
+        return cell
     }
     
-    private let label: UILabel = {
-        let view = UILabel()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.text = "Secondary"
-        return view
-    }()
-    
-    override func loadView() {
-        self.view = UIView()
-        self.view.backgroundColor = .white
-        self.setupSubviews()
+    private func loadImageAsync(from imageURL: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: imageURL), let image = UIImage(data: data) {
+                DispatchQueue.main.async { [weak self] in
+                    self?.eventImageView?.image = image
+                    self?.tableView.reloadData()
+                }
+            }
+        }
     }
     
-    private func setupSubviews() {
-        self.setupLabel()
-    }
+    // MARK: - EventDetailViewControllerDelegate
     
-    private func setupLabel() {
-        self.view.addSubview(label)
-        NSLayoutConstraint.activate([
-            self.label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            self.label.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
-        ])
+    func didSetData() {
+        DispatchQueue.main.async { [weak self] in
+            self?.tableView.reloadData()
+        }
     }
 }
--- TMEvents/Features/Detail/EventDetailViewModel.swift
+++ TMEvents/Features/Detail/EventDetailViewModel.swift
@@ -7,6 +7,52 @@
 
 import Foundation
 
+protocol EventDetailViewControllerDelegate: NSObject {
+    func didSetData()
+}
+
 class EventDetailViewModel {
     
+    enum RowType {
+        case name, date, venue, location
+        
+        var title: String {
+            switch self {
+            case .name:
+                return "Name"
+            case .date:
+                return "Date"
+            case .venue:
+                return "Vanue"
+            case .location:
+                return "Location"
+            }
+        }
+    }
+    
+    struct Row {
+        var value: String
+        var rowType: RowType
+    }
+    
+    weak var delegate: EventDetailViewControllerDelegate?
+    
+    private (set) var items: [Row] = []
+    
+    func setData(for event: Event) {
+        items = [
+            .init(value: event.name, rowType: .name),
+            .init(value: event.formattedDate, rowType: .date)
+        ]
+        
+        if let venueName = event.venueName {
+            items.append(.init(value: venueName, rowType: .venue))
+        }
+        
+        if let location = event.location {
+            items.append(.init(value: location, rowType: .location))
+        }
+        
+        delegate?.didSetData()
+    }
 }
